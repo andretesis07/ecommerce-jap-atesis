00703b24cd0ce410d7330d919b9d3e5497756fe9
Andre <andretesis07@gmail.com>
2021-08-22 18:24:02 -0300

    Actualizacion_entrega1

--- js/login.js
+++ js/login.js
@@ -1,3 +1,27 @@
+function conectar(){
+    let dato = {}
+    let usuario = document.getElementById('usuario').value;
+    let email = document.getElementById('email').value;
+  
+    // Verifico si los campos están vacios
+    if(usuario.trim() === '' || email.trim() === ''){
+      alert('Usuario o Email vacio/s');
+    }else{
+      dato.usuario = usuario;
+      dato.email = email;
+      dato.estado = true;
+      // Guardo el objeto en el localStorage
+      localStorage.setItem('dato',JSON.stringify(dato));
+      location.href = "./index.html";
+    }
+  }
+  
+  function desconectar(){
+    localStorage.clear();
+    location.href = "./inicio.html";
+  }
+  
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
