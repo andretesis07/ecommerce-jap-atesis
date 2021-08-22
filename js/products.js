87cce715b3287d18210226389b53a71899c53217
Andre <andretesis07@gmail.com>
2021-08-22 17:13:38 -0300

    Entrega1_parte2

--- js/products.js
+++ js/products.js
@@ -1,6 +1,51 @@
+var categoriesArray = [];
+
+function showCategoriesList(array){
+    showSpinner();
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let category = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ category.name +`</h4>
+                        <small class="text-muted">` + "Vendidos: " + category.soldCount + ` artículos</small>
+                    </div>
+                        <hr></hr>
+                        <p class="mb-1">`+ category.description +`</p>
+                        <br> 
+                        <b> <p class="mb-1">`+ category.currency + " " + category.cost +`</p> </b>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+    hideSpinner();
+}
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showCategoriesList(categoriesArray);
+        }
+    });
 });
