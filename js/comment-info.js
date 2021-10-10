506c84224d1de2ef6c49d616516349af44f8bfb8
Andre <andretesis07@gmail.com>
2021-10-10 01:39:55 -0300

    Entrega 3 - casi casi

--- js/comment-info.js
+++ js/comment-info.js
@@ -1,15 +1,35 @@
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok") {
-            comment = resultObj.data;
+var commentsArray = [];
 
-            let commentUserHTML = document.getElementById("commentUser");
-            let commentDescriptionHTML = document.getElementById("commentDescription");
-            let commentScoreHTML = document.getElementById("commentScore");
-            
-            commentUserHTML.innerHTML = comment.user;
-            commentDescriptionHTML.innerHTML = comment.description;
-            commentScoreHTML.innerHTML = comment.score;
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            commentsArray = resultObj.data;
+            //Muestro los comentarios ordenados
+            showComments(commentsArray);
         }
     });
 });
+
+function showComments(array){
+    showSpinner();
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let comments = array[i];
+
+        htmlContentToAppend += `
+        <a class="list-group-item list-group-item-action">
+            <div class="row">
+            <p class="card-text"></p>
+                    <p class="text-center"> El usuario: `+ comments.user +` dijo: </p>
+                    <br>
+                    <p class="text-center"> "`+ comments.description +`"</p>
+                    <br>
+                    <p class="text-center"> Puntuacion: `+ comments.score +`<span class="fa fa-star"></span>" </p> 
+            </div>
+        </a>
+        `
+        document.getElementById("comment-list-container").innerHTML = htmlContentToAppend;
+    }
+    hideSpinner();
+}
