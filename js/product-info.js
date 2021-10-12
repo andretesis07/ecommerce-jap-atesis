7f630cb74bf5566638c6dfe29e056182124c88b6
Andre <andretesis07@gmail.com>
2021-10-12 01:08:40 -0300

    Entrega 4 - casi terminada

--- js/product-info.js
+++ js/product-info.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function(e){
             let productCurrencyHTML = document.getElementById("productCurrency");
             let productSoldCountHTML = document.getElementById("productSoldCount");
             let productImagesHTML = document.getElementById("productImages");
+            let productRelatedProducts = document.getElementById("relatedProducts")
 
             productNameHTML.innerHTML = product.name;
             productDescriptionHTML.innerHTML = product.description;
@@ -19,6 +20,49 @@ document.addEventListener("DOMContentLoaded", function(e){
             productCurrencyHTML.innerHTML = product.currency;
             productSoldCountHTML.innerHTML = product.soldCount;
             productImagesHTML.innerHTML = product.images;
+            productRelatedProducts.innerHTML = product.relatedProducts;
         }
     });
 });
+
+var relatedArray = [];
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productsArray = resultObj.data;
+            //Muestro las categorías ordenadas
+
+            firstProduct = productsArray[1];
+            secondProduct = productsArray[3];
+
+            
+            relatedArray = commentsArray.push(firstProduct);
+            relatedArray = commentsArray.push(secondProduct);
+
+            showRelatedProductsList(productsArray);
+        }
+    });
+});
+
+function showRelatedProductsList(array){
+    showSpinner();
+    let htmlContentToAppend = "";
+    
+        let products1 = array[1];
+
+        htmlContentToAppend += `
+        <a href="products.html" class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + products1.imgSrc + `" class="img-thumbnail">
+                    <h4 class="mb-1">`+ products1.name +`</h4>
+                </div>
+            </div>
+        </a>
+        `
+        document.getElementById("prod-related-list-container").innerHTML = htmlContentToAppend;
+    
+    hideSpinner();
+}
